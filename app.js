8ba15552617149b8c1285a27b0b2675c3841ce4d
Opeyemi <opeadetola@gmail.com>
2024-04-11 20:51:57 -0400

    events in node

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const EventEmitter = require("events");
 
+const customEmitter = new EventEmitter();
+customEmitter.on("response", () => {
+  console.log(`data received`);
+});
+
+customEmitter.emit("response");
