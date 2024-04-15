d0cd7942d5dd4a79e4dd115b304fcd9dd229747c
Opeyemi <opeadetola@gmail.com>
2024-04-15 10:18:55 -0400

    upd eventsEmitter

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ customEmitter.on("response", () => {
   console.log(`data received`);
 });
 
+customEmitter.on("response", () => {
+  console.log(`I can put some other logic here`);
+});
+
 customEmitter.emit("response");
